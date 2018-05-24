03a4a13369e5b91072e248aca1b59be825bd7d8f
khadija-rahman <khadija.a13@gmail.com>
2018-05-24 20:46:53 +0100

    table for movies

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<?xml version="1.0" encoding="UTF-8"?>
+<movies>
+<movie>
+    <title>Dr No</title>
+    <actor>Sean Connery</actor>
+    <start_time>9</start_time>
+    <end_time>11</end_time>
+  </movie>
+  <movie>
+    <title>Thunderball</title>
+    <actor>Sean Connery</actor>
+    <start_time>11</start_time>
+    <end_time>13</end_time>
+  </movie>
+  <movie>
+    <title>Moonraker</title>
+    <actor>Roger Moore</actor>
+    <start_time>9</start_time>
+    <end_time>11</end_time>
+  </movie>
+  <movie>
+    <title>GoldenEye</title>
+    <actor>Pierce Brosnan</actor>
+    <start_time>9</start_time>
+    <end_time>11</end_time>
+  </movie>
+  <movie>
+    <title>Tomorrow Never Dies</title>
+    <actor>Pierce Brosnan</actor>
+    <start_time>13</start_time>
+    <end_time>15</end_time>
+  </movie>
+</movies>
