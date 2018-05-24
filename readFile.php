b4583f4a5289e3f778dc654208d913c504c07130
Natalia Fernández <natalia.fpintos@gmail.com>
2018-05-24 20:48:27 +0100

    Populate table code

--- readFile.php
+++ readFile.php
@@ -9,9 +9,29 @@ foreach ($xmlFile->children() as $movie) {
     $movies[] = $movie;
     foreach ($movie as $key => $value) {
         echo ucwords($key), ": $value", PHP_EOL;
-        // echo ucwords($key), ': ', $book->$key, PHP_EOL;
+         echo ucwords($key), ': ', $movie->$key, PHP_EOL;
     }
     echo PHP_EOL;
 }
 
 var_dump($movies);
+?>
+
+<html>
+    <table>
+        <tr>
+            <td>Channel</td>
+            <td>Hours</td>
+        </tr>
+
+        <?php
+        echo "hello";
+        foreach($movies as $movie) {
+            echo '<tr>';
+            echo '<td>', $movie->actor, '</td>';
+            echo '<td>', $movie->title, '</td>';
+            echo '</tr>';
+        }
+        ?>
+    </table>
+</html>
