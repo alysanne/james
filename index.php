5ba32220cdbb4e87c291f4f33bca46aded72c69f
[annaecc] <[annaeccleston79@gmail.com]>
2018-05-24 21:03:06 +0100

    Fixed file upload

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<movies>
-<movie>
-    <title>Dr No</title>
-    <actor>Sean Connery</actor>
-    <start_time>9</start_time>
-    <end_time>11</end_time>
-  </movie>
-  <movie>
-    <title>Thunderball</title>
-    <actor>Sean Connery</actor>
-    <start_time>11</start_time>
-    <end_time>13</end_time>
-  </movie>
-  <movie>
-    <title>Moonraker</title>
-    <actor>Roger Moore</actor>
-    <start_time>9</start_time>
-    <end_time>11</end_time>
-  </movie>
-  <movie>
-    <title>GoldenEye</title>
-    <actor>Pierce Brosnan</actor>
-    <start_time>9</start_time>
-    <end_time>11</end_time>
-  </movie>
-  <movie>
-    <title>Tomorrow Never Dies</title>
-    <actor>Pierce Brosnan</actor>
-    <start_time>13</start_time>
-    <end_time>15</end_time>
-  </movie>
-</movies>
+<?php
+
+$filename = 'Movies.xml';
+
+$xmlFile = simplexml_load_file($filename) or die("Error loading XML file.\n");
+
+foreach ($xmlFile->children() as $movie) {
+
+    $movies[] = $movie;
+    foreach ($movie as $key => $value) {
+        echo ucwords($key), ": $value", PHP_EOL;
+         echo ucwords($key), ': ', $movie->$key, PHP_EOL;
+    }
+    echo PHP_EOL;
+}
+
+var_dump($movies);
+?>
+
+<html>
+    <table>
+        <tr>
+            <td>Channel</td>
+            <td>Hours</td>
+        </tr>
+
+        <?php
+        echo "hello";
+        foreach($movies as $movie) {
+            echo '<tr>';
+            echo '<td>', $movie->actor, '</td>';
+            echo '<td>', $movie->title, '</td>';
+            echo '</tr>';
+        }
+        ?>
+    </table>
+</html>
